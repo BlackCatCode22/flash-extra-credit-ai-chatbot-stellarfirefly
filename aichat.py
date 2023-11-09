9a5d611fbd78f595cfe4b6e336f16dcfd142b126
Jay C. Parangalan <stellarfirefly@yahoo.com>
2023-11-08 20:39:13 -0800

    added conversation support
    - added conversation support
    - added new button to reset conversation
    - named buttons

--- aichat.py
+++ aichat.py
@@ -15,40 +15,47 @@ message_history = []
 # this is what happens when someone starts at the root of the website "/"
 @app.route("/", methods=["GET", "POST"])
 def start_here():
+    global message_history
+    
     if request.method == "POST":
-        # There is a <textarea> on the index.html webpage named "question"
-        #   what the user types in the <textarea> named "question" will be used as the prompt for text-davinci-003 .
-        text_question = request.form.get("question")
+        if request.form.get("reset") != None:
+            message_history = []
+            return render_template("index.html", textQuestion="hello", textAnswer="(A new conversation has been started.)")
 
-        # Choose the model from OpenAI that you want to use.
-        model_name = "gpt-3.5-turbo"
+        if request.form.get("prompt") != None:
+            # There is a <textarea> on the index.html webpage named "question"
+            #   what the user types in the <textarea> named "question" will be used as the prompt for text-davinci-003 .
+            text_question = request.form.get("question")
 
-        # Make a request to gpt-3.5-turbo.
-        try:
-            new_message_list = message_history
-            new_message_list.append({
-                "role": "user",
-                "content": text_question,
+            # Choose the model from OpenAI that you want to use.
+            model_name = "gpt-3.5-turbo"
+
+            # Make a request to gpt-3.5-turbo.
+            try:
+                new_message_list = message_history
+                new_message_list.append({
+                    "role": "user",
+                    "content": text_question,
+                })
+                
+                # Call the method named create from the Completion class of the OpenAI Python client library.
+                response = openai.ChatCompletion.create(
+                    model = model_name,
+                    messages = new_message_list,
+                    max_tokens = 1000
+                )
+
+            except openai.error.OpenAIError as e:
+                print(f"Something unexpected happened. Here is a debugging clue: {e}")
+
+            text_answer = response["choices"][0]["message"]["content"]
+            message_history.append({
+                "role": "assistant",
+                "content": response["choices"][0]["message"]["content"],
             })
-            
-            # Call the method named create from the Completion class of the OpenAI Python client library.
-            response = openai.ChatCompletion.create(
-                model = model_name,
-                messages = new_message_list,
-                max_tokens = 1000
-            )
 
-        except openai.error.OpenAIError as e:
-            print(f"Something unexpected happened. Here is a debugging clue: {e}")
+            return render_template("index.html", textQuestion=text_question, textAnswer=text_answer)
 
-        text_answer = response["choices"][0]["message"]["content"]
-        message_history.append({
-            "role": "assistant",
-            "content": response["choices"][0]["message"]["content"],
-        })
-        print(message_history)     # for debugging
-
-        return render_template("index.html", textQuestion=text_question, textAnswer=text_answer)
     return render_template("index.html", textQuestion="", textAnswer="")
 
 if __name__ == "__main__":
